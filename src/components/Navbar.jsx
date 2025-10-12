4430cfda83de767d3605e3afcfd0505fd5488ffc
Arpit Paliwal <paliwalarpit93@gmail.com>
2025-10-12 23:21:05 +0530

    added home route on my calendar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,10 @@ const Navbar = ()=>{
             <header className='w-full bg-zinc-800 sm:px-6 md:px-8 lg:px-12'>
                 <nav className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between'>
                     <div className="h-28 sm:h-24 md:h-22 lg:h-16 flex items-center">
+                        <Link to="/" className='flex flex-row'>
                         <img src={CalendarIcon} className='h-8 w-8 mr-2' alt="" />
                         <div className='text-white font-semibold text-lg font-sans' >My Calendar</div>
+                        </Link>
                     </div>
                     <div className='hidden md:flex items-center space-x-4'>
                         <Link to="/" className='text-zinc-100 hover:text-zinc-300'>
